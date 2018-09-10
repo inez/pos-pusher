9a026fec8aa1dd9c0347e38b8f284bd5071ec3a4
Inez Korczynski <inez@squareup.com>
2018-09-10 15:46:58 -0700

    ...

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const SECRET_API_KEY = process.env.SECRET_API_KEY;
-const port = 3535;
+const port = process.env.PORT || 5000
 const express = require('express');
 const app = express();
 const Pushy = require('pushy');
