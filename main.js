188dfdce5c34488b8c1a5ba0eb03c615573c92d3
Inez Korczynski <korczynski@gmail.com>
2018-09-10 18:34:19 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const SECRET_API_KEY = process.env.SECRET_API_KEY;
+const port = 3535;
+const express = require('express');
+const app = express();
+const Pushy = require('pushy');
+const pushyAPI = new Pushy(SECRET_API_KEY);
+
+app.get('/sendRequest/:device_token/:amount_cents', (req, res) => {
+	var data = {
+		amount_cents: parseInt(req.params.amount_cents)
+	};
+	pushyAPI.sendPushNotification(data, [req.params.device_token], {}, function (err, id) {
+		if (err) {
+			console.log('Fatal Error', err);
+			res.send('Fatal Error');
+		} else {
+			console.log('Push sent successfully! (ID: ' + id + ')');
+			res.send(id);
+		}
+	});
+});
+app.listen(port, () => console.log('Example app listening on port ' + port + '!'))
+
